a602c6a8d77f447061c9986c63e2e5c386202861
information extraction <123042919+zmy-0316@users.noreply.github.com>
2024-08-24 16:08:28 +0800

    Update huiwen_str.py

--- backtracking_/segment_question/huiwen_str.py
+++ backtracking_/segment_question/huiwen_str.py
@@ -1,1 +1,23 @@
+"""
+分割回文子串
+"""
 
+class Solution:
+    def combine(self, s):
+        result = []
+        self.backtracking(s, 0, [], result)
+        return result
+    def backtracking(self, s, startindex, path, result):
+        if startindex == len(s):
+            result.append(path[:])
+            return
+        for i in range(startindex, len(s)):
+            if s[startindex: i + 1] == s[startindex: i + 1][::-1]:
+                path.append(s[startindex: i + 1])
+                self.backtracking(s, i + 1, path, result)
+                path.pop()
+
+
+
+solution = Solution()
+print(solution.combine("aab"))
